cab2b1c5a890b6a14ab8c725675aac7390d81ede
Zach Robinson <zprobinson@sio.midco.net>
2020-12-06 15:05:05 -0600

    injected recycler functionality

--- CleanDownloads/Worker.cs
+++ CleanDownloads/Worker.cs
@@ -11,18 +11,22 @@ namespace CleanDownloads
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly FileRecycler _recycler;
+        private const int _weekDelay = 1000 * 60 * 60 * 24 * 7; // one week in milliseconds
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, FileRecycler recycler)
         {
             _logger = logger;
+            _recycler = recycler;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
+                _recycler.Recycle();
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                await Task.Delay(_weekDelay, stoppingToken);
             }
         }
     }
