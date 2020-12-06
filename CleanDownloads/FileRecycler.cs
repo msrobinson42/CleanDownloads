cef0859e01929bc70f75ee55c0e1f78eb93a687e
Zach Robinson <zprobinson@sio.midco.net>
2020-12-06 15:04:25 -0600

    cleanup

---
+++ CleanDownloads/FileRecycler.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanDownloads
+{
+    public class FileRecycler
+    {
+        private readonly IConfiguration _config;
+        private readonly FileExpiration _expiration;
+
+        public FileRecycler(IConfiguration config, FileExpiration expiration)
+        {
+            _config = config;
+            _expiration = expiration;
+        }
+
+        public void Recycle()
+        {
+            var directory = _config.GetValue<string>("Directory");
+            var files = Directory.GetFiles(directory);
+            var expDate = _expiration.GetExpirationDate();
+
+            foreach(var file in files)
+            {
+                var accessTime = File.GetLastAccessTime(file);
+
+                if (accessTime.IsEarlierThan(expDate))
+                {
+                    file.FileRecycle();
+                }
+            }
+        }
+    }
+}
