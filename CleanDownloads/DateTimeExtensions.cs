538676d504de89959fe38208dbdd6ec4cacac296
Zach Robinson <zprobinson@sio.midco.net>
2020-12-06 15:03:49 -0600

    cleanup

---
+++ CleanDownloads/DateTimeExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanDownloads
+{
+    static class DateTimeExtensions
+    {
+        public static bool IsEarlierThan(this DateTime @this, DateTime date)
+        {
+            return @this < date;
+        }
+
+        public static bool IsLaterThan(this DateTime @this, DateTime date)
+        {
+            return @this > date;
+        }
+    }
+
+    static class DateTimeOffsetExtensions
+    {
+        public static bool IsEarlierThan(this DateTimeOffset @this, DateTimeOffset date)
+        {
+            return @this < date;
+        }
+
+        public static bool IsLaterThan(this DateTimeOffset @this, DateTimeOffset date)
+        {
+            return @this > date;
+        }
+    }
+}
